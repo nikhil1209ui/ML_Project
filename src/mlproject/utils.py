117ef2c8041031c9e1b86ee0cb6a868ba4226ce6
“[Nikhil <[nikhil1209ui@github.com]>
2024-07-25 16:22:02 +0530

    data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,8 @@ from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
+import pickle
+import numpy as np
 
 load_dotenv()
 
@@ -21,10 +23,23 @@ def read_sql_data():
             password = password,
             db = db
         )
-        logging.info("Connection Established", mydb)
+        logging.info("Connection Established: %s", mydb)
         df = pd.read_sql_query('Select * from students', mydb)
         print(df.head())
 
         return df
     except Exception as ex:
         raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
